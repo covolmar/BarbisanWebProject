39149ada3b2cf97a0c6d698325aecb83424eb86f
covolmar <marco.covolan@bluewin.ch>
2019-08-15 12:54:59 +0200

    Projektdateien hinzufügen.

---
+++ BarbisanWebProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BarbisanWebProject.MvcApplication" Language="C#" %>
---
+++ BarbisanWebProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BarbisanWebProject.Startup))]
+namespace BarbisanWebProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
