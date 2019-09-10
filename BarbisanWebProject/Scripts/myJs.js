a2348c8ce87b50f6a6a045fc45bf0bd5b9ab28e5
covolmar <marco.covolan@bluewin.ch>
2019-09-10 18:50:43 +0200

    clean up code

--- BarbisanWebProject/Scripts/myJs.js
+++ BarbisanWebProject/Scripts/myJs.js
@@ -1,17 +1,5 @@
-﻿//// When the user scrolls the page, execute myFunction
-//window.onscroll = function () { myFunction() };
+﻿"use-strict"
 
-//// Get the header
-//var navbar = document.getElementById("navbar");
-
-//// Get the offset position of the navbar
-//var sticky = navbar.offsetTop;
-
-//// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-//function myFunction() {
-//	if (window.pageYOffset > sticky) {
-//		navbar.classList.add("sticky");
-//	} else {
-//		navbar.classList.remove("sticky");
-//	}
-//}
+window.fadeIn = function (obj) {
+    $(obj).fadeIn(1000);
+}
